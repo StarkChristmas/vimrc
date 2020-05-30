8af4cb42dfa79a3af11b5174a36c3c9a08b68fba
Hex Christmas <hacker@Hexs-MacBook-Pro.local>
2020-05-31 00:03:15 +0800

    2020.05.31

---
+++ README.md
@@ -0,0 +1,26 @@
+# Vimrc
+![](screenshots/4.png)
+## 支持开发语言：
+```
+C
+C++
+Python
+```
+## 一键安装
+```
+bash <(curl https://github.com/HexChristmas/vimrc/raw/master/autoinstall.sh -L)
+```
+## 添加MarkdownPreview
+```
+sudo npm -g install instant-markdown-d
+```
+## 快捷键
+```
+\cc 注释光标所在行
+\ci 切换突出显示的行的状态
+\cu 取消注释光标突出显示的行
+\cs 使用/ *和* /而不是普通的//注释突出显示的内容
+```
+![](screenshots/3.gif)
+![](screenshots/1.png)
+![](screenshots/2.png)
---
+++ autoinstall.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cd
+git clone https://github.com/HexChristmas/vimrc
+cd
+
+if [ -e oldvim ]
+then
+	rm -Rf oldvim
+fi
+
+if [ -e .vim ]
+then
+	#if .vim exist, check if .vimrc exists
+	if [ -e .vimrc ]
+	#if both exist, create a dir and move both
+	then
+		mkdir -p oldvim
+		mv .vim oldvim/.vim
+		mv .vimrc oldvim/.vimrc
+	#if only .vim exists
+	else
+		mkdir -p oldvim
+		mv .vim oldvim/.vim
+	fi
+else
+	#if only .vimrc exists
+	if [ -e .vimrc ]
+	then
+		mkdir -p oldvim
+		mv .vimrc oldvim/.vimrc
+	fi
+fi
+
+cd
+cd vimrc1
+mv -f .vimrc ..
+cd
+rm -Rf vimrc
+mkdir -p .vim/bundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo '\n' | vim +PluginInstall +qall
